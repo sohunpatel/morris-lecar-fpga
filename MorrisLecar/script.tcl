40cd707ee7fd67f5794f7c56c3ddc7d5eab72688
sohunpatel <sohunpatel@gmail.com>
2022-03-14 16:03:25 -0400

    Update build scripts

--- MorrisLecar/script.tcl
+++ MorrisLecar/script.tcl
@@ -5,13 +5,13 @@
 ############################################################
 open_project MorrisLecar
 set_top MorrisLecar
-add_files MorrisLecar/src/MorrisLecar.cpp
-add_files MorrisLecar/src/MorrisLecar.h
+add_files /home/sohun/Krembil/MorrisLecar/src/MorrisLecar.cpp
+add_files /home/sohun/Krembil/MorrisLecar/src/MorrisLecar.h
 open_solution "MorrisLecar" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -output /home/sohun/IP/MorrisLecar.zip -rtl verilog
-source "./MorrisLecar/MorrisLecar/directives.tcl"
+source "/home/sohun/Krembil/MorrisLecar/MorrisLecar/directives.tcl"
 #csim_design
 csynth_design
 #cosim_design
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-/tool/Xilinx/Vitis/2020.1/bin/vitis_hls ./MorrisLecar/script.tcl
+/tools/Xilinx/Vitis/2020.1/bin/vitis_hls ./MorrisLecar/script.tcl
+rm -rf ~/IP/MorrisLecar
+unzip ~/IP/MorrisLecar.zip -d ~/IP/MorrisLecar
+rm -rf ~/IP/MorrisLecar.zip

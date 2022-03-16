5e9f5f9b2c2fa85589a1f70056ba335577f813df
sohunpatel <sohunpatel@gmail.com>
2022-03-16 11:35:46 -0400

    Update run script to have date changing

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+timedatectl set-ntp 0
+timedatectl set-time 2020-04-16
 /tools/Xilinx/Vitis/2020.1/bin/vitis_hls ./MorrisLecar/script.tcl
 rm -rf ~/IP/MorrisLecar
 unzip ~/IP/MorrisLecar.zip -d ~/IP/MorrisLecar
 rm -rf ~/IP/MorrisLecar.zip
+timedatectl set-ntp 1

3308f326e4668b63900055d07de94160ef4664be
sohunpatel <sohunpatel@gmail.com>
2022-04-01 10:44:54 -0400

    Update scripts and gitignore for file structure

--- MorrisLecar/MorrisLecar/script.tcl
+++ MorrisLecar/MorrisLecar/script.tcl
@@ -5,14 +5,14 @@
 ############################################################
 open_project MorrisLecar
 set_top MorrisLecar
-add_files /home/sohun/Krembil/MorrisLecar/src/MorrisLecar.cpp
-add_files /home/sohun/Krembil/MorrisLecar/src/MorrisLecar.h
+add_files /home/sohun/Krembil/MorrisLecar/MorrisLecar/src/MorrisLecar.cpp
+add_files /home/sohun/Krembil/MorrisLecar/MorrisLecar/src/MorrisLecar.h
 open_solution "MorrisLecar" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
-config_export -format ip_catalog -output /home/sohun/IP/MorrisLecar.zip -rtl verilog
-source "/home/sohun/Krembil/MorrisLecar/MorrisLecar/directives.tcl"
+config_export -format ip_catalog -output /home/sohun/Krembil/MorrisLecar/IP/MorrisLecar.zip -rtl verilog
+source "/home/sohun/Krembil/MorrisLecar/MorrisLecar/MorrisLecar/directives.tcl"
 #csim_design
 csynth_design
 #cosim_design
-export_design -flow impl -rtl verilog -format ip_catalog -output /home/sohun/IP/MorrisLecar.zip
+export_design -flow impl -rtl verilog -format ip_catalog -output /home/sohun/Krembil/MorrisLecar/IP/MorrisLecar.zip
--- MorrisLecar/run.sh
+++ MorrisLecar/run.sh
@@ -1,7 +1,7 @@
 timedatectl set-ntp 0
 timedatectl set-time 2020-01-01
 /tools/Xilinx/Vitis/2020.1/bin/vitis_hls ./MorrisLecar/script.tcl
-rm -rf ~/IP/MorrisLecar
-unzip ~/IP/MorrisLecar.zip -d ~/IP/MorrisLecar
-rm -rf ~/IP/MorrisLecar.zip
+rm -rf ../IP/MorrisLecar
+unzip ../IP/MorrisLecar.zip -d ../IP/MorrisLecar
+rm -rf ../IP/MorrisLecar.zip
 timedatectl set-ntp 1
